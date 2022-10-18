a38030e25d00f7b94aa25663292fc6f7ecbb9447
Yehuda Nevo <45956551+YehudaNevo@users.noreply.github.com>
2022-10-18 13:14:37 +0300

    sphere 1

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -8,6 +8,7 @@ void Renderer::Render()
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
 			glm::vec2 coord = {(float) x /(float) m_FinalImage->GetWidth() , (float)y/(float) m_FinalImage->GetHeight()};
+			coord = coord * 2.0f - 1.0f; // 0 -> 1 --> -1 -> 1 
 			m_ImageData[ x + y * m_FinalImage->GetWidth() ]  =  PerPixel(coord);
 			
 		}
@@ -19,8 +20,31 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 {
 	uint8_t r = (uint8_t)(coord.x * 255.0f);
 	uint8_t g = (uint8_t)(coord.y * 255.0f);
+	float radius = 0.5f;
 
-	return 0xff000000 | (g << 8) | r;
+	glm::vec3 RayOrigin(0.0f,0.0f,2.0f);
+	glm::vec3 RayDirection(coord.x, coord.y, -1.0f);
+
+	//RayDirection = glm::normalize(RayDirection);
+
+	// a = Ray origin 
+	// b = Ray direction 
+	// r = rudius
+	// t = hit distance 
+
+	float a = glm::dot(RayDirection, RayDirection); //RayDirection.x * RayDirection.x + RayDirection.y * RayDirection.y + RayDirection.z * RayDirection.z;
+	float b = 2.0f * glm::dot(RayOrigin, RayDirection);
+	float c = glm::dot(RayOrigin, RayOrigin) - radius * radius;
+	float discriminant = b * b - 4.0f * a * c;
+
+	if (discriminant >= 0.0f)
+	{
+		return 0xffff00ff;
+	}
+	else
+	{
+		return 0xff000000;
+	}
 }
 
 void Renderer::OnResize(uint32_t width, uint32_t height)
